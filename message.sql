9567bb25ee944863de3c18f4caf958f670c74ab1
Ubuntu <ubuntu@ip-172-31-89-188.ec2.internal>
2025-03-17 08:14:17 +0000

    Modified

--- message.sql
+++ message.sql
@@ -1,4 +1,20 @@
-CREATE TABLE messages (
+-- Create database if not exists
+CREATE DATABASE IF NOT EXISTS mydb;
+
+-- Switch to the newly created database
+USE mydb;
+
+-- Create user if not exists
+CREATE USER IF NOT EXISTS 'admin'@'%' IDENTIFIED BY 'admin';
+
+-- Grant privileges to the user
+GRANT ALL PRIVILEGES ON mydb.* TO 'admin'@'%' WITH GRANT OPTION;
+
+-- Flush privileges
+FLUSH PRIVILEGES;
+
+-- Create table
+CREATE TABLE IF NOT EXISTS messages (
     id INT AUTO_INCREMENT PRIMARY KEY,
     message TEXT
 );
